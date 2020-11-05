698f68d3258f82f0fc223e049e93262561b07fd6
Simon McEnlly <simon.mcenlly@gmail.com>
2020-11-05 10:54:53 +1000

    Remove debug/trace and reformat wallaby config file

--- wallaby.js
+++ wallaby.js
@@ -6,35 +6,32 @@ module.exports = function (wallaby) {
     // (except external files),
     // as they should not be loaded in browser,
     // their wrapped versions will be loaded instead
-		files: [
-			{ pattern: 'src/**/*.js', load: false },
-			{ pattern: 'src/**/*.ts', load: false },
-			{ pattern: 'src/**/*.html', load: false },
-			{ pattern: 'webpack-config/*.js', load: false },
-			{ pattern: 'test/karma/helpers.js', load: false },
-			{ pattern: 'test/karma/runtimeError.js', load: false },
-			{ pattern: 'test/karma/simulateMaskedRuntimeError.js', load: false },
+    files: [
+      { pattern: 'src/**/*.js', load: false },
+      { pattern: 'src/**/*.ts', load: false },
+      { pattern: 'src/**/*.html', load: false },
+      { pattern: 'webpack-config/*.js', load: false },
+      { pattern: 'test/karma/helpers.js', load: false },
+      { pattern: 'test/karma/runtimeError.js', load: false },
+      { pattern: 'test/karma/simulateMaskedRuntimeError.js', load: false },
 
-			// ignore test files since they get declared in the "tests" option
-			{ pattern: 'src/**/test/e2e/**/*.{js,ts}', ignore: true },
-			{ pattern: 'src/**/test/midway/**/*.{js,ts}', ignore: true },
-			{ pattern: 'src/**/test/unit/**/*.helpers.{js,ts}', ignore: true },
-			{ pattern: 'src/**/*.spec.ts', ignore: true }
-		],
+      // ignore test files since they get declared in the "tests" option
+      { pattern: 'src/**/test/e2e/**/*.{js,ts}', ignore: true },
+      { pattern: 'src/**/test/midway/**/*.{js,ts}', ignore: true },
+      { pattern: 'src/**/test/unit/**/*.helpers.{js,ts}', ignore: true },
+      { pattern: 'src/**/*.spec.ts', ignore: true },
+    ],
 
-		tests: [
-			{ pattern: 'src/**/test/unit/**/!(*.helpers).{js,ts}', load: false },
-			{ pattern: 'src/**/*.spec.ts', load: false }
-		],
+    tests: [
+      { pattern: 'src/**/test/unit/**/!(*.helpers).{js,ts}', load: false },
+      { pattern: 'src/**/*.spec.ts', load: false },
+    ],
 
     postprocessor: wallaby.postprocessors.webpack(options),
 
     setup: function () {
       // required to trigger test loading
       window.__moduleBundler.loadTests();
-		},
-
-		debug: true,
-		trace: true
+    },
   };
 };
